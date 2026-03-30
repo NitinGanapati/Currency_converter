712e8121de6a3383f902b3f3673090498b203704
Nitin G B <nitingb961@gmail.com>
2026-03-30 16:03:02 +0530

    Update2

--- lib/functions/fetchrates.dart
+++ lib/functions/fetchrates.dart
@@ -8,14 +8,10 @@ Future<RatesModel> fetchRates()  async {
   final response = await http.get(Uri.parse('https://openexchangerates.org/api/latest.json?app_id=cbf1bf00e33547348cc0fc153ab5dea2'));
   print(response.body);
   final result = ratesModelFromJson(response.body);
-
-
   return result;
-
 }
 
-Future<Map> fetchcurrencies() async{
-  
+Future<Map> fetchcurrencies() async {
   final response = await http.get(Uri.parse('https://openexchangerates.org/api/currencies.json?app_id=cbf1bf00e33547348cc0fc153ab5dea2'));
   final allCurrencies = allCurrenciesFromJson(response.body);
   return allCurrencies;
