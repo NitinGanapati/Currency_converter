6276f671f460fb8d0fd29812a742f3f9c4ff85c9
Nitin G B <nitingb961@gmail.com>
2026-03-31 17:11:51 +0530

    yety

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,16 +9,21 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+plugins {
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
